253bc91627b1463cfc36e492bf1a214b084389c1
Артём Ситников <sitnikov.artem91@gmail.com>
2023-12-13 10:26:30 +0200

    add block effective

--- src/components/EffectiveProgram/index.jsx
+++ src/components/EffectiveProgram/index.jsx
@@ -1,58 +1,41 @@
 import React from 'react'
-import { FaFileDownload } from "react-icons/fa";
-import { MdTimer } from "react-icons/md";
-import { MdEnergySavingsLeaf } from "react-icons/md";
-import { FaMicrophoneAlt } from "react-icons/fa";
-import { ImSpoonKnife } from "react-icons/im";
-import { GiPresent } from "react-icons/gi";
-import Elips from '../../assets/icon/Elips';
+import { useTranslation } from 'react-i18next';
+import { AiOutlineCaretRight } from "react-icons/ai";
 
+import { AiFillFire } from "react-icons/ai";
 
 const mainEffects=[
     {
         id:1,
-        text:'Авторська программа тренувань Анны Бурнышевой',
-        icon:<FaFileDownload className='w-[30px] h-[30px]'/>
+        text:"aboutTextFirst",
     },
     {
         id:2,
-        text:'Тренування до 15 хвилин на день (Табата, EMOM, WOD)',
-        icon:<MdTimer className='w-[30px] h-[30px]'/>
+        text:'aboutTextSecond',
     },
     {
         id:3,
-        text:'Мотивація та підтримка від ментора або Анны',
-        icon:<MdEnergySavingsLeaf className='w-[30px] h-[30px]'/>
+        text:'aboutTextTheerd',
     },
     {
         id:4,
-        text:'Прямі ефіри експертів: психолог, реабілітолог, дієтолог та інші',
-        icon:<FaMicrophoneAlt className='w-[30px] h-[30px]'/>
+        text:'aboutTextFourth',
     },
-    {
-        id:5,
-        text:'2 види меню з рецептами від дієтолога на весь марафон',
-        icon:<ImSpoonKnife className='w-[30px] h-[30px]'/>
-    },
-    {
-        id:6,
-        text:'Нагороди за найкращі результати',
-        icon:<GiPresent className='w-[30px] h-[30px]'/>
-    },
+ 
 
 ]
 
 function EffectiveProgram() {
+    const { t } = useTranslation();
   return (
-    <section className='px-3 py-10 border-black flex flex-col items-center justify-center relative'>
-        <h2 className='pb-10 text-center text-[24px]'>Секрет нашого підходу</h2>
-        {/* <Elips  /> */}
+    <section className='px-3 py-10 border-black flex flex-col justify-between items-center  relative h-[90vh]'>
+        <h2 className='pb-30 text-center text-[24px]'> {t("description.aboutIntensiveTitle")}</h2>
         <div className='grid grid-cols-1 sm:grid-cols-2 gap-7 px-2'>
      
            {mainEffects.map(post=>(
-            <div key={post.id} className='flex gap-4 justify-center items-center max-w-[500px]'>
-                {post.icon}
-                <p className='flex-1 text-[20px]'>{post.text}</p>
+            <div key={post.id} className='flex gap-6 justify-center items-center max-w-[500px]'>
+           <AiOutlineCaretRight className='w-[30px] h-[30px] text-btnColor'/>
+                <p className='flex-1 text-[18px]'> {t(`description.${post.text}`)}</p>
             </div>
            ))}
         </div>
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -11,14 +11,14 @@ function Hero() {
         <span className="text-[24px] pb-[20px] px-2">
           {t("description.titleHero")}
         </span>
-        <h1 className=" text-btnColor font-semibold text-4xl pt-[10px] pb-[50px]">
+        <h1 className=" text-btnColor font-semibold text-4xl pt-[10px] pb-[30px]">
           {t("description.subTitleHero")}
         </h1>
         <p className="pb-[30px] text-[20px]"> {t("description.descriptionHero")}</p>
 
         <div className="flex flex-col items-center">
           <div className="w-[100px] h-[100px] rounded-full bg-[url('./assets/profilePhoto.jpg')] bg-cover bg-top border-[4px] " />
-          <strong className="text-center text-[20px] font-medium pt-[30px]"> {t("description.AnnName")}</strong>
+          <strong className="text-center text-[20px] font-medium pt-[30px] "> {t("description.AnnName")}</strong>
         </div>
       </div>
 
@@ -29,7 +29,7 @@ function Hero() {
           <img src={arrowRight} alt="arrow right" className="animate-pulse" />
         </div>
       </div>
-      <p className="text-black/30 text-[14px]">
+      <p className="text-black/30 text-[14px] pt-[5px]">
         {t("description.descriptionSloganHero")}
       </p>
     </section>
