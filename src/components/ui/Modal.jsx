33a32304cd9064cc80ed14465650e636ab2150ad
Артём Ситников <sitnikov.artem91@gmail.com>
2023-12-15 10:54:55 +0200

    add modal and styles for it

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -1,14 +1,28 @@
+import { FiX } from "react-icons/fi";
+import ActiveBlock from "./ActiveBlock";
+
 function Modal({setShowModal}) {
 
   return (
     <>
     <div className=" fixed top-0 left-0 w-[100%] h-[100%] bg-black/70"/>
-    <div className="text-white fixed top-[100px]">
-    <button  onClick={()=>{
+    <div className="text-white top-0 left-0  fixed w-[100%] h-[100%] flex justify-center items-center ">
+      <div className=" bg-[url('./assets/modal_bg.jpg')] relative bg-cover bg-top rounded-xl w-[370px] h-[200px]  ">
+      <div  onClick={()=>{
         setShowModal(false)
         document.body.style.overflow='visible'
-        }}>click</button>
+        }}>
+          <FiX className="w-10 h-10 absolute right-0 top-[-40px]" />
+        </div>
+        <div className=" flex flex-col items-center justify-between gap-10">
+          <h2 
+           style={{textShadow:'#FFF 3px 2px 15px'}}
+          className="text-[18px] text-black/40 text-center pt-4">Не зволікай. Доєднуйся до марафону прямо зараз. </h2>
+          <ActiveBlock/>
+        </div>
     </div>
+      </div>
+    
     </>
   )
 }
